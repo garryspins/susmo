cbbc920358e493f4d8670a01e1f7d33da05a05d0
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2021-07-07 18:34:01 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,104 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./!res/main.css">
+    <title>Susmo: Home</title>
+</head>
+<body>
+    <div class="header">
+        <div class="topbar">
+            <div class="logo">
+                <img src="./!res/mono-i-mean-logo.png">
+                Susmo
+            </div>
+            <div class="item">Piss off</div>
+        </div>
+        <div class="content">
+            <div class="container">
+                Susmo
+                <div class="inner">When the imposter is sus!!!</div>
+            </div>
+        </div>
+    </div>
+    <div class="main">
+        <div class="stats">
+              <div class="stat">
+                0
+                <div class="head">Friends</div>
+            </div>
+            <div class="stat">
+                0
+                <div class="head">Sexual Partners</div>
+            </div>
+            <div class="stat">
+                0
+                <div class="head">Imaginary Friends</div>
+            </div>
+        </div>
+        <a class="join" href="/">Test</a>
+        <div class="box">
+            <div class="head">
+                here are our things
+                <div class="one">go away</div>
+            </div>
+            <br>
+            <div class="flex">
+                <div class="item">
+                    <img src="./!res/mono-i-mean-logo.png">
+                    <div class="head">header</div>
+                    <div class="desc">description</div>
+                </div>
+                <div class="item">
+                    <img src="./!res/mono-i-mean-logo.png">
+                    <div class="head">booba</div>
+                    <div class="desc">we have big boob</div>
+                </div>
+                <div class="item">
+                    <img src="./!res/mono-i-mean-logo.png">
+                    <div class="head">anti gay</div>
+                    <div class="desc">not gay like xenin discord server!!!</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="head">
+                uhh people
+                <div class="one">yea... people</div>
+            </div>
+            <br>
+            <div class="flex">
+            <div class="item">
+                    <img src="./!res/melon.png">
+                    <div class="head">Melon</div>
+                    <div class="desc" style="color:red;">owner</div>
+                </div>
+                <div class="item">
+                    <img src="./!res/hoofy.gif">
+                    <div class="head">hoofy</div>
+                    <div class="desc" style="color:green;">basically a furry</div>
+                </div>
+                <div class="item">
+                    <img src="./!res/biwwy.gif">
+                    <div class="head">billy</div>
+                    <div class="desc" style="color:pink;">local femboy</div>
+                </div>
+                <div class="item">
+                    <img src="./!res/luca.png">
+                    <div class="head">not billy (luca)</div>
+                    <div class="desc" style="color:orange;">moral support</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer">
+            copyright xenin discord 202021 @ youtube.com
+        </div>
+    </div>
+</body>
+</html>
+
